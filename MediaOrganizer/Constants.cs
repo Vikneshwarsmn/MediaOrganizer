8b21150771f1c7a648d9ef4c8549bd448a57a3b9
Vikneshwar <s.vikneshwar@outlook.com>
2022-04-25 22:29:31 +0530

    Updated FileType constants

--- MediaOrganizer/Constants.cs
+++ MediaOrganizer/Constants.cs
@@ -2,8 +2,8 @@
 {
     public class FileType
     {
-        public const string Photo = "Photo";
-        public const string Video = "Video";
+        public const string Photo = "Photos";
+        public const string Video = "Videos";
         public const string None = "None";
     }
 }
