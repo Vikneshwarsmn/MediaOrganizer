c93f4b4d348f8177f4cbf9ae0d8561995d3cf682
Vikneshwar <s.vikneshwar@outlook.com>
2022-04-25 20:52:36 +0530

    Created Configurations and Constants

---
+++ FileOrganizer/Configurations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Configuration;
+
+namespace FileOrganizer
+{
+    public class Configurations
+    {
+        public static string PhotosSourcePath = ConfigurationManager.AppSettings["PhotosSourcePath"].ToString();
+        public static string PhotosOrganizeBasePath = ConfigurationManager.AppSettings["PhotosOrganizeBasePath"].ToString();
+        public static bool IsCopyEnabled = Convert.ToBoolean(ConfigurationManager.AppSettings["IsCopyEnabled"].ToString());
+    }
+}
---
+++ FileOrganizer/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace FileOrganizer
+{
+    public class FileType
+    {
+        public const string Photo = "Photo";
+        public const string Video = "Video";
+        public const string None = "None";
+    }
+}
